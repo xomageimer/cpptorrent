496b34e51fab1c5421e2aab5e1eb38fc2af735a5
xomaq <xomageimer@Gmail.com>
2022-02-19 13:54:04 +0300

    add logger#2

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -32,26 +32,42 @@ public:
     void reset(std::ostream & new_os) { os_ptr = &new_os; }
     ~Logger() {
         out("Logger deleted");
+        f_log.close();
     }
 private:
     Logger() {
+#ifdef FILE_LOGGER
+        ss << std::chrono::duration_cast<std::chrono::seconds>(
+                last_action_time.time_since_epoch()).count();
+        f_log.open(std::string("out_" + ss.str() + ".log"), std::ios::out | std::ios::binary);
+        os_ptr = &f_log;
+#endif
         out("Logger created");
     };
     void outtime();
     std::mutex m;
     std::stringstream ss;
+    std::fstream f_log;
     std::ostream * os_ptr = &std::cerr;
     std::chrono::steady_clock::time_point last_action_time = std::chrono::steady_clock::now();
 };
 
-#ifdef LOGGER
+#ifdef FILE_LOGGER
+#define LOG(...)                 \
+Logger::log().out(__VA_ARGS__)
+#define RESET(os)
+#endif
+#ifdef CONSOLE_LOGGER
 #define LOG(...)                 \
 Logger::log().out(__VA_ARGS__)
 #define RESET(os)                \
 Logger::log().reset(os);
-#else
+#endif
+#ifndef CONSOLE_LOGGER
+#ifndef FILE_LOGGER
 #define LOG(...)
 #define RESET(os)
+#endif
 #endif
 
 #endif //CPPTORRENT_LOGGER_H
